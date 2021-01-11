d21145fbbf7b8156f59a9172b3c61c59b056dc31
tedmartins <tedmartins.me@gmail.com>
2021-01-11 14:28:50 -0300

    pequena correção

--- dinogame/scripts/script.js
+++ dinogame/scripts/script.js
@@ -71,18 +71,3 @@ function createCactus() {
 
 createCactus();
 document.addEventListener('keyup', handleKeyUp);
-
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-        this.sound.play();
-    }
-    this.stop = function(){
-        this.sound.pause();
-    }    
-}
