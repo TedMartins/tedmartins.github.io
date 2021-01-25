1ee5fc08dcde62991d0b06c9daf53caee1c4d5bc
tedmartins <tedmartins.me@gmail.com>
2021-01-25 09:34:05 -0300

    add food

--- challenges/DIO-Snake-Game/assets/js/script.js
+++ challenges/DIO-Snake-Game/assets/js/script.js
@@ -6,6 +6,11 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = 'right';
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function background() {
     context.fillStyle = '#333030';
@@ -19,5 +24,46 @@ function addSnake(){
     }
 }
 
-background();
-addSnake();
+function addFood(){
+    context.fillStyle = 'f2f2f2';
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction = 'left';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 39 && direction != 'left') direction = 'right';
+    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
+function startGame(){
+    if(snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+    background();
+    addSnake();
+    addFodd();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == 'right') snakeX += box;
+    if(direction == 'left') snakeX -= box;
+    if(direction == 'up') snakeY -= box;
+    if(direction == 'down') snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
