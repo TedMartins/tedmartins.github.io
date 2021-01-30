3940034f431c19af7955686b4b1a72f5f684028c
tedmartins <tedmartins.me@gmail.com>
2021-01-30 15:36:00 -0300

    movendo

---
+++ challenges/DIO-dinogame/README.md
@@ -0,0 +1,17 @@
+# Dinossaur Game
+
+## About
+
+This is a simple project based on the T-Rex Runner game. This project was part of the Bootcamp offered by GFT on the Digital Innovation One platform, and was guided by Celso Henrique.
+
+## Customization
+
+I added a button to replay the game after game over and also added a score. For the template, I  used [Bulma.io](https://bulma.io) framework.
+
+## Demo
+
+You can see the project running at:
+
+[https://tedmartins.github.io/dinogame/](https://tedmartins.github.io/dinogame/)
+
+Enjoy!
---
+++ challenges/DIO-dinogame/scripts/script.js
@@ -0,0 +1,76 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let isJumping = false;
+let isGameOver = false;
+let position = 0;
+let points = 0;
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        if(!isJumping) {
+            jump();
+        }
+    }
+}
+
+function jump() {
+    isJumping = true;
+    
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            // jump down
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                    position -=20
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+            // jump up
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 5000;
+    
+        if (isGameOver) return;
+    
+    cactus.classList.add('cactus');
+    background.appendChild(cactus);
+    cactus.style.left = cactusPosition + 'px';
+
+    let leftInterval = setInterval(() => {
+        // remove cactus 
+        if (cactusPosition < -30) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+            points = points + 10;
+            document.getElementById("points").innerHTML = points;
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) { 
+            // Game 0ver
+            clearInterval(leftInterval);
+            isGameOver = true;
+            document.getElementById("game-over").innerHTML = '<div id="game-over"><h2 class="subtitle is-size-1 is-bold has-text-centered mb-0"">Game Over<p id="newfont" class="subtitle is-size-6 is-bold has-text-centered">Your Score is: ' + points + ' points</p><button class="play" onClick="window.location.reload();">Play Again</button></h2></div>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20)
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+document.addEventListener('keyup', handleKeyUp);
