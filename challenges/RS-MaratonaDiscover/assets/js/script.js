2b71fb5e8cd1cf4ad4ea2d1e8cc9852ae28a57d4
tedmartins <tedmartins.me@gmail.com>
2021-01-31 21:27:39 -0300

    adicionada função para salvar e exibir os dados

--- challenges/RS-MaratonaDiscover/assets/js/script.js
+++ challenges/RS-MaratonaDiscover/assets/js/script.js
@@ -35,13 +35,11 @@ const Transaction = {
 
     add(transaction){
         Transaction.all.push(transaction)
-
         App.reload()
     },
 
     remove(index) {
         Transaction.all.splice(index, 1)
-
         App.reload()
     },
 
@@ -76,14 +74,11 @@ const DOM = {
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
-
         DOM.transactionsContainer.appendChild(tr)
     },
     innerHTMLTransaction(transaction) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
-
         const amount = Utils.formatCurrency(transaction.amount)
-
         const html = `
         
         <td class="description">${transaction.description}</td>
@@ -117,28 +112,22 @@ const DOM = {
 const Utils = {
     formatAmount(value){
         value = Number(value) * 100
-        
         return value
     },
 
     formatDate(date){
         const splittedDate = date.split("-")
-
         return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
     },
 
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
-
         value = String(value).replace(/\D/g, "")
-
         value = Number(value) / 100
-
         value = value.toLocaleString("pt-BR", {
             style: "currency",
             currency: "BRL"
         })
-
         return signal + value
     }
 }
@@ -158,7 +147,6 @@ const Form = {
 
     validateFields() {
         const { description, amount, date } = Form.getValues()
-        
         if( description.trim() === "" || 
             amount.trim() === "" || 
             date.trim() === "") {
@@ -168,26 +156,34 @@ const Form = {
 
     formatValues() {
         let { description, amount, date } = Form.getValues()
-
         amount = Utils.formatAmount(amount)
-
         date = Utils.formatDate(date)
-
         return {
-            
+            description,
+            amount,
+            date
         }
     },
 
+    saveTransaction(transaction) {
+        Transaction.add(transaction)
+    },
+
+    clearFields() {
+        Form.description.value = ""
+        Form.amount.value = ""
+        Form.date.value = ""
+    },
 
     submit(event) {
         event.preventDefault()
 
         try {
             Form.validateFields()
-            Form.formatValues() 
-            // 2:22:43
-            //
-            //
+            const transaction = Form.formatValues() 
+            Form.saveTransaction(transaction) 
+            Form.clearFields()
+            Modal.close()
         } catch (error) {
             alert(error.message)
         }
@@ -201,7 +197,6 @@ const App = {
         })
         
         DOM.updateBalance()
-        
         
     },
     reload() {
